885f3453016de58041be477b15f731f9d7000a2b
Matt Barbour <barbour@zoology.ubc.ca>
2013-10-14 18:29:06 -0700

    created a function to calculate and visualize phenotypic and genetic correlations
    conducts mantel test to examine correlation between phenotypic and
    genetic correlations. (but is this the best method?)

---
+++ pheno_geno_correlations.R
@@ -0,0 +1,30 @@
+# Calculates phenotypic and genetic correlations among trait variables and combines them into one large matrix.  User can specify whether the phenotypic or genetic correlations go on top.
+# Author: Matt Barbour
+# Date: October 14, 2013
+
+# need to think about writing a method for ensuring the data columns correspond exactly to each other.
+pheno_geno_correlations <- function(phenotypic_data, genotype_data, plot_data){
+  library(psych)
+  library(gdata)
+  library(vegan)
+  library(ggplot2)
+  
+  x_corr_data <- corr.test(phenotypic_data)
+  y_corr_data <- corr.test(genotype_data)
+  
+  x_corr_euclid_dist <- vegdist(x_corr_data$r,"euclidean")
+  y_corr_euclid_dist <- vegdist(y_corr_data$r, "euclidean")
+  
+  print(mantel(x_corr_euclid_dist, y_corr_euclid_dist, method = "pearson", permutations = 10000))
+  
+  x_corr_data$r[upper.tri(x_corr_data$r)] <- y_corr_data$r[upper.tri(y_corr_data$r)] # keeps the phenotypic correlation matrix in the lower left triangle and the genotypic correlations in the upper right triangle (assuming x = phenotypic data and y = genotypic data)
+  print(x_corr_data$r) 
+  
+  # create heat map of correlation data set of users choice
+  melt_plot <- melt(cor(plot_data, use="pairwise.complete.obs"))
+  melt_plot$X1 <- factor(melt_plot$X1, levels=unique(melt_plot$X1), ordered=TRUE)
+  melt_plot$X2 <- factor(melt_plot$X2, levels=unique(melt_plot$X2), ordered=TRUE)
+  
+  (plot_corr_plot <- qplot(data=melt_plot, x=X2, y=X1, fill=value, geom="tile") + scale_fill_gradientn(colours=c("blue","white", "red"), values = c(-1,0,1)) + xlab("") + ylab("") + theme(axis.text.x = element_text(angle=90))) # its weird that I have a value of 1 corresponding with "white" but that is not reflected in the figure legend.  The figure is representing what I want it to thoug
+}
+
--- stepAIC_comparison.R
+++ stepAIC_comparison.R
@@ -1,9 +1,8 @@
-AIC Model Comparison with stepAIC
-=================================
+############  AIC Model Comparison with stepAIC
+# The function below will take output from stepAIC and create a data frame with AIC values, differences between models, likelihood of models, and model weights for inferring the "best" subset of models (based on 95% confidence).
+# Author: Matt Barbour
+# Date: October 14, 2013
 
-The function below will take output from stepAIC and create a data frame with AIC values, differences between models, likelihood of models, and model weights for inferring the "best" subset of models (based on 95% confidence).
-
-```{r}
 stepAIC_comparison <- function(stepAIC_model) {
   stepAIC_models <- as.character(stepAIC_model$anova$Step) # converts to character
   stepAIC_models[1] <- "null model (+ 1)" # inputs null model formula
@@ -11,9 +10,5 @@ stepAIC_comparison <- function(stepAIC_model) {
   delta <- AIC_values - min(AIC_values) # calculate AIC difference between models
   likelihood <- round(exp(-0.5*delta),3) # calculate likelihood of each model
   weights <- round(likelihood/sum(likelihood),3) # calculate the weight of each model
-  #cumulative_weights <- sort(cumsum(weights[length(weights):1]), decreasing=TRUE)
   cbind.data.frame(stepAIC_models, AIC_values, delta, likelihood, weights)
 }
-```
-
-
